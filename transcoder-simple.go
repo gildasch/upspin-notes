e66948321509bb49b167bcf880b48ef703962361
Gildas <gildaschbt@gmail.com>
2018-02-03 01:46:35 +0100

    almost good code for pres

--- transcoder-simple.go
+++ transcoder-simple.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -13,12 +14,27 @@ import (
 )
 
 func main() {
-	cfg, err := config.FromFile("config")
+	cfg, err := config.FromFile("config.fosdem")
 	if err != nil {
 		panic(err)
 	}
 
 	cl := client.New(cfg)
+
+	http.HandleFunc("/list/", func(w http.ResponseWriter, r *http.Request) {
+		path := strings.TrimPrefix(r.URL.Path, "/list/")
+		des, err := cl.Glob(path + "/*")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+
+		json.NewEncoder(w).Encode(des)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
 
 	// e.g. GET /augie@upspin.io/Images/camstream.mp4
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -38,3 +54,40 @@ func main() {
 	fmt.Println("Listening on :8080...")
 	http.ListenAndServe(":8080", nil)
 }
+
+// func otherHandlers() {
+// 		http.HandleFunc("/inspect/", func(w http.ResponseWriter, r *http.Request) {
+// 		path := strings.TrimPrefix(r.URL.Path, "/inspect/")
+// 		de, err := cl.Lookup(upspin.PathName(path), false)
+// 		if err != nil {
+// 			if uerrors.Is(uerrors.NotExist, err) {
+// 				fmt.Fprintf(w, "%q does not exist", path)
+// 				return
+// 			}
+// 			json.NewEncoder(w).Encode(err)
+// 			http.Error(w, err.Error(), http.StatusNotFound)
+// 			return
+// 		}
+// 		json.NewEncoder(w).Encode(de)
+// 		if err != nil {
+// 			http.Error(w, err.Error(), http.StatusInternalServerError)
+// 			return
+// 		}
+// 	})
+
+// 	http.HandleFunc("/list/", func(w http.ResponseWriter, r *http.Request) {
+// 		path := strings.TrimPrefix(r.URL.Path, "/list/")
+// 		des, err := cl.Glob(path + "/*")
+// 		if err != nil {
+// 			http.Error(w, err.Error(), http.StatusNotFound)
+// 			return
+// 		}
+
+// 		json.NewEncoder(w).Encode(des)
+// 		if err != nil {
+// 			http.Error(w, err.Error(), http.StatusInternalServerError)
+// 			return
+// 		}
+// 	})
+
+// }
